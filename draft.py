36e96906b6bf924f1c3e4b3cd9ae9075c17181e6
FNafisa <43866925+FNafisa@users.noreply.github.com>
2025-02-15 22:55:45 +0300

    Update draft.py

--- draft.py
+++ draft.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("Part 13: Connections and Secrets")
 
 st.header("1. Connection and Secret Management")
-st.write('API', st.secrets.OPENAI_API_KEY)
+st.write('user', st.secrets.db_username)
 
 
 st.write(st.secrets.OPENAI_API_KEY)
