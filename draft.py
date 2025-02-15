bd432695271e323d48122f64123bbdf169b8f58e
FNafisa <43866925+FNafisa@users.noreply.github.com>
2025-02-15 22:53:28 +0300

    Create draft.py

---
+++ draft.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title("Part 13: Connections and Secrets")
+
+st.header("1. Connection and Secret Management")
+st.write('API', st.secrets.OPENAI_API_KEY)
+
+
+st.write(st.secrets.OPENAI_API_KEY)
